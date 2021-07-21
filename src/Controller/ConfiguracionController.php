dc199c4e0ae0e634e86d6bf05d527004186503d6
Michael Rodriguez Alvarez <michaelrodriguezalvarez@gmail.com>
2021-07-20 23:01:18 -0500

    Creando opción de cambio de clave para el usuario. closed #29

--- src/Controller/ConfiguracionController.php
+++ src/Controller/ConfiguracionController.php
@@ -2,19 +2,50 @@
 
 namespace App\Controller;
 
+use App\Form\ConfiguracionType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class ConfiguracionController extends AbstractController
 {
     /**
-     * @Route("/configuracion", name="configuracion")
+     * @Route("/configuracion", name="configuracion", methods={"GET","POST"})
      */
-    public function index(): Response
+    public function index(Request $request, UserRepository $userRepository, UserPasswordEncoderInterface $passwordEncoder): Response
     {
+        $user = $userRepository->find($this->getUser()->getId());
+        $form = $this->createForm(ConfiguracionType::class, $user);
+        $form->handleRequest($request);
+
+        $errors = $form->getErrors(true);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            
+            if($form->get('password')->getData() != null) {
+                $user->setPassword(
+                    $passwordEncoder->encodePassword(
+                        $user,
+                        $form->get('password')->getData()
+                    )
+                );
+                $entityManager->persist($user);
+            }           
+
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Su configuración fue guardada correctamente.');
+            return $this->redirectToRoute('configuracion');
+        }
+
         return $this->render('configuracion/index.html.twig', [
-            'error' => null,
+            'errors' => $errors,
+            'user'=> $user,
+            'form' => $form->createView(),
         ]);
     }
 }
