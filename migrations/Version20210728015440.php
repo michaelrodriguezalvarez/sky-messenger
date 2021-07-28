95f1548edff7bf4f6fca69140844b1932819c08b
Michael Rodriguez Alvarez <michaelrodriguezalvarez@gmail.com>
2021-07-27 20:55:53 -0500

    Actualizando las estructuras de las tablas

--- migrations/Version20210728015440.php
+++ migrations/Version20210728015440.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210723030713 extends AbstractMigration
+final class Version20210728015440 extends AbstractMigration
 {
     public function getDescription() : string
     {
